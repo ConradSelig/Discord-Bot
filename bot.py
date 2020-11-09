cf8a9cc45f7e14b65e2205935dce9784c3786e2f
ConradSelig <conrad.selig@mines.sdsmt.edu>
2020-11-08 21:41:16 -0700

    Adding the "list roles" command.

--- bot.py
+++ bot.py
@@ -20,7 +20,9 @@ async def on_ready():
             break
 
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='you ʕ•ᴥ•ʔ'))
-    print("Connected.\n\n")
+    print("Connected.")
+    print("Guilds:", client.guilds)
+    print("\n==============================\n")
 
 @client.event
 async def on_message(message):
@@ -36,5 +38,30 @@ async def on_message(message):
 
     if message.content == "pog count":
         await message.channel.send("I currently know " + str(len(os.listdir("./Pog_Images/"))) + " different ways to Pog.")
+
+    if message.content == "list roles":
+        guild = client.get_guild(message.guild.id)
+        all_roles = guild.roles
+        roles = []
+
+        for role in all_roles:
+            if str(role.color) == "#000000" and role.name != "@everyone":
+                roles.append(role)
+
+        response = "";
+        
+        if len(roles) == 0:
+            response = "This server has no general roles."
+        elif len(roles) == 1:
+            response += guild.name + " only has 1 role.\n"
+            for i, role in enumerate(roles):
+                response += "\t" + str(i+1) + ". " + role.name + "\n"
+        else:
+            response += guild.name + " currently has " + str(len(roles)) + " roles.\n"
+            for i, role in enumerate(roles):
+                response += "\t" + str(i+1) + ". " + role.name + "\n"
+
+        print("Printing roles. (" + str(len(roles)) + " roles printed)")
+        await message.channel.send(response)
 
 client.run(TOKEN)
